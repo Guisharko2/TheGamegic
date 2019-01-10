3c41633986a13babe0ea1d1e09c7bafd32077cb5
Gallier Guillaume <guillaume.gallier@sfr.fr>
2019-01-10 16:16:06 +0100

    mis à jour

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,8 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\RequestOptions;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,25 +20,53 @@ class HomeController extends AbstractController
 {
 
     /**
-     * @Route("/not", name="not", methods={"GET"})
+     * @Route("/not_found", name="not_found", methods={"GET"})
      */
-    public function not(): Response
+    public function not(int $next=1): Response
     {
+        $client = new Client([
+            RequestOptions::HTTP_ERRORS => false,
+        ]);
+
+        $url = '&unique=card&include_multilingual=true&format=image&sas=grid&order=name&page=';
+        $search = '';
+        if ($_GET){
+            $search=$_GET['search'];
+        }
+        $nameCard = $client->request('GET', $this->uri . $search . $url . $next);
+        $statusCode = $nameCard->getStatusCode();
+        if ($statusCode > 400) {
+            return $this->redirectToRoute('not_found');
+        }
+        if(isset($_GET['search'])) {
+
+            return $this->redirectToRoute('searchpage', ['search' => $_GET['search'], 'next' => $next, ]);
+        }
+
         return $this->render('homepage/not_found.html.twig');
     }
     /**
      * @Route("/", name="homepage", methods={"GET"})
      */
-    public function index(int $page=1): Response
+    public function index(int $next=1): Response
     {
-        $client = new Client();
-        $url = '&unique=card&sas=grid&order=name&page=';
+        $client = new Client([
+            RequestOptions::HTTP_ERRORS => false,
+        ]);
 
+        $url = '&unique=card&include_multilingual=true&format=image&sas=grid&order=name&page=';
+        $search = '';
+        if ($_GET){
+            $search=$_GET['search'];
+        }
+        $nameCard = $client->request('GET', $this->uri . $search . $url . $next);
+        $statusCode = $nameCard->getStatusCode();
+        if ($statusCode > 400) {
+            return $this->redirectToRoute('not_found');
+        }
         if(isset($_GET['search'])) {
-            $search = $_GET['search'];
-            $nameCard = $client->request('GET', $this->uri .$search. $url. $page);
 
-            return $this->redirectToRoute('searchpage', ['search' => $_GET['search'], 'page' => 1, ]);
+            return $this->redirectToRoute('searchpage', ['search' => $_GET['search'], 'next' => $next, ]);
         }
 
         return $this->render('homepage/index.html.twig');
@@ -45,34 +75,70 @@ class HomeController extends AbstractController
     private $uri = 'https://api.scryfall.com/cards/search?q=';
 
     /**
-     * @Route("/searchpage/{search}/{page}", name="searchpage", methods={"GET"},requirements={"page"})
+     * @Route("/searchpage/{search}/{next}", name="searchpage", methods={"GET"},requirements={"next"})
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function apiRequest(string $search, int $page=1): Response
+    public function basicResearch(string $search, Request $request, PaginatorInterface $paginator, int $next=1): Response
     {
 
-        $client = new Client();
-        $url = '&unique=card&sas=grid&order=name&page=';
+        $client = new Client([
+            RequestOptions::HTTP_ERRORS => false,
+        ]);
 
-        $nameCard = $client->request('GET', $this->uri . $search . $url . $page);
+        $url = '&unique=card&include_multilingual=true&format=image&sas=grid&order=name&page=';
+        $nameCard = $client->request('GET', $this->uri . $search . $url .$next);
         $statusCode = $nameCard->getStatusCode();
-        dump($statusCode);
         if ($statusCode > 300) {
-                return $this->redirectToRoute('not');
+                return $this->redirectToRoute('not_found');
         }
         if(isset($_GET['search'])) {
 
-                return $this->redirectToRoute('searchpage', ['search' => $_GET['search'], 'page' => 1, ]);
+                return $this->redirectToRoute('searchpage', ['search' => $_GET['search'], 'next' => $next]);
         }
         $body = $nameCard->getBody();
         $json = json_decode($body->getContents(), true);
-
+        $cardsPages = $json['data'];
+        $cardsPages = $paginator->paginate(
+            $cardsPages,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            20
+        );
+        $cardsPages->setPageRange(9);
 
         return $this->render('homepage/search.html.twig', [
             'cards' => $json['data'],
             'search' => $search,
-            'total' => $json['total_cards']
-
+            'next' => $next,
+            'total' => $json['total_cards'],
+            'cardsPages' => $cardsPages,
         ]);
+    }
+
+    /**
+     * @Route("/advancedSearchpage/", name="advanced_search", methods={"GET"})
+     */
+    public function advancedResearch(int $next=1): Response
+    {
+        $client = new Client([
+            RequestOptions::HTTP_ERRORS => false,
+        ]);
+
+        $url = '&unique=card&include_multilingual=true&format=image&sas=grid&order=name&page=';
+        $search = '';
+        if ($_GET){
+            $search=$_GET['search'];
+        }
+        $nameCard = $client->request('GET', $this->uri . $search . $url . $next);
+        $statusCode = $nameCard->getStatusCode();
+        if ($statusCode > 400) {
+            return $this->redirectToRoute('not_found');
+        }
+        if(isset($_GET['search'])) {
+
+            return $this->redirectToRoute('searchpage', ['search' => $_GET['search'], 'next' => $next, ]);
+        }
+        return $this->render('homepage/advanced_search.html.twig');
     }
 }
